2467f3a2b070c33756ec20ff1eb7320978e4c33e
Edward Banner <edward.banner@gmail.com>
2024-10-24 14:18:36 -0400

    LibWeb: Garbage collect m_contained_children

--- Userland/Libraries/LibWeb/Layout/Box.cpp
+++ Userland/Libraries/LibWeb/Layout/Box.cpp
@@ -31,7 +31,7 @@ Box::~Box()
 void Box::visit_edges(Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
-    visitor.visit(m_contained_abspos_children);
+    visitor.visit(m_contained_children);
 }
 
 JS::GCPtr<Painting::Paintable> Box::create_paintable() const
